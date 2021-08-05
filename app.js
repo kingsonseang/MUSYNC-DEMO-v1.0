9addd244729d84a0cb427f7078411c77ef5d32c5
kingsonseang <kingsonseang@gmail.com>
2021-08-05 11:38:31 +0100

    commit

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const cover = document.getElementById('cover')
 const playbtn = document.getElementById('play')
 const prevbtn = document.getElementById('prev')
 const nextbtn = document.getElementById('next')
+const mutebtn = document.getElementById('mute')
+const volumebtn = document.getElementById('volume')
 const musicCon = document.getElementById('controls')
 const progress = document.getElementById('progress')
 const progressContainer = document.getElementById('progressCon')
+const elapsed = document.getElementById('elapsed')
+const totalduration = document.getElementById('tduration')
 
 
 const songs = ['Black Catcher', '321', 'lemonade', 'crabRave']
@@ -92,6 +96,32 @@ function updateProgress(e) {
     const progPrntValue = `${progressPercent}%`
 
     progress.style.width = progPrntValue
+    
+    // // current time
+    // let currentMin = Math.floor(currentTime / 60);
+    // let currentSec = Math.floor(currentTime % 60);
+    // if(currentSec < 10) {
+    //     currentSec = `0${currentSec}`
+    // }
+
+    // elapsed.innerText = `${currentMin}:${currentSec}`;
+
+    // let totalMin = math.floor(audio.duration / 60);
+
+    if(audio.duration){
+        var curmins = Math.floor(audio.currentTime / 60);
+        var cursec = Math.floor(audio.currentTime - curmins * 60);
+        var durmins = Math.floor(audio.duration / 60);
+        var dursec = Math.floor(audio.duration - durmins * 60);
+        if(cursec < 10){
+            cursec = `0${cursec}`
+        }
+        elapsed.innerText = `${curmins}:${cursec}`;
+        totalduration.innerText = `${durmins}:${dursec}`;
+    } else {
+        elapsed.innerText = `0:00`;
+        totalduration.innerText = `0:00`;
+    }
 }
 
 function setProgress(e) {
@@ -108,11 +138,19 @@ progressContainer.addEventListener('click', setProgress)
 audio.addEventListener('ended', nextSong)
 
 
-function onElementFocused(e)
-{
-    if (e && e.target)
-        document.activeElement = e.target == document ? null : e.target;
-} 
 
-if (document.addEventListener) 
-    document.addEventListener("focus", onElementFocused, true);
+// mute
+function mute() {
+    if(audio.muted){
+        audio.muted = false
+        volumebtn.querySelector('i.fas').classList.remove("fa-volume-mute");
+        volumebtn.querySelector('i.fas').classList.add("fa-volume-off");
+    } else {
+        audio.muted = true;
+        volumebtn.querySelector('i.fas').classList.add("fa-volume-mute");
+        volumebtn.querySelector('i.fas').classList.remove("fa-volume-off");
+    }
+}
+
+mutebtn.addEventListener('click', mute)
+
